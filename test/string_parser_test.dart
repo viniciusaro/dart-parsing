74f7ad42d418a21e22ef4e762e8c06e8b7ddc8f0
viniciusaro <vinicius.a.ro@gmail.com>
2026-01-30 10:51:44 -0300

    string acute tests

--- test/string_parser_test.dart
+++ test/string_parser_test.dart
@@ -27,4 +27,29 @@ void main() {
     expect(result.toString(), "Lorem ipsum\n");
     expect(rest.toString(), "End");
   });
+
+  test(
+      "acute strings are not automatically made equivalent by parsers, "
+      "since dart string representation does not consider different "
+      "enconded chars to be equal ", () {
+    const bsb = "BSB";
+    const iAcute0 = "í"; // String.fromCharCodes([0x00ED]);
+    const iAcute1 = "í"; // String.fromCharCodes([0x0069, 0x0301]);
+
+    final input = [
+      "Bras${iAcute0}lia",
+      "Bras${iAcute1}lia",
+    ];
+
+    final cityParser = OptionalParser(
+      OneOf([StringLiteral("Bras${iAcute0}lia").map((_) => bsb)]),
+    );
+
+    final result = input
+        .map((city) => cityParser.run(MutableStringSlice(city)))
+        .map((tuple) => tuple.$1)
+        .toList();
+
+    expect(result, [bsb, null]);
+  });
 }
